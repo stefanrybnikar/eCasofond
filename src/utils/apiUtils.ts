bd3e4bc0c5ff438125bca320f70a3bcf912f3941
JanSkla <jan.skladal11@gmail.com>
2023-06-07 10:34:06 +0200

    apiUtils

---
+++ src/utils/apiUtils.ts
@@ -0,0 +1,24 @@
+const fetchUrl = "http://localhost:8080" // setup env
+
+export const get = async (link: string) => {
+    return await fetch(fetchUrl + link,
+    {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+    }).then(response => response.json())
+}
+
+export const post = async (link: string, data: object) => {
+    return await fetch(fetchUrl + link,
+        {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        }).then(response => response.json())
+}
